949c1f3ef495c249e372b7c33f417a400c59b6c5
dpTablo <leeyw2355@gmail.com>
2019-10-15 09:27:19 +0900

    Spring Boot, Gradle init

---
+++ src/main/java/dptablo/spring/configuration/MainConfiguration.java
@@ -0,0 +1,10 @@
+package dptablo.spring.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "dptablo.springboot_react")
+public class MainConfiguration {
+
+}
---
+++ src/main/java/dptablo/spring/controller/PageController.java
@@ -0,0 +1,15 @@
+package dptablo.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/page")
+public class PageController {
+    @RequestMapping("/hello")
+    public String helloPage(Model model) { 
+        model.addAttribute("param1", "dptablo");
+        return "hello";
+    }
+}
